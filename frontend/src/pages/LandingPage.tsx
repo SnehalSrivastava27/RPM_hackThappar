30d6bdbccf6fe9570d07d88c5a36d0a1e1b0b803
Snehal Srivastava <118104081+SnehalSrivastava27@users.noreply.github.com>
2025-02-08 16:26:48 +0530

    Button Landing Page Changed

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -67,16 +67,16 @@ function LandingPage() {
       {/* buttons  */}
       <div className="flex justify-between mx-5">
   <button 
-    onClick={() => navigate("/doctor")} 
+    onClick={() => navigate("/login")} 
     className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
   >
-    Go to Doctor Dashboard
+    Login
   </button>
   <button 
-    onClick={() => navigate("/patient")} 
+    onClick={() => navigate("/signup")} 
     className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600"
   >
-    Go to Patient Dashboard
+    Signup
   </button>
 </div>
 
